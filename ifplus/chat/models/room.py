c60b67dafc898fa428fe3c7a6d99de3df07599d3
caoke <kcao@libnet.sh.cn>
2016-11-30 19:32:51 +0800

    Chat 完善

--- ifplus/chat/models/room.py
+++ ifplus/chat/models/room.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
+from ifplus.restful.patched import fields
 from ifplus.res.models.file import FileObject
 
 
 class ChatRoom(FileObject):
+    """聊天室对象"""
+
     def __init__(self, room_path, **kwargs):
         super(ChatRoom, self).__init__(room_path, **kwargs)
+
+    def owner(self):
+        pass
+
+    def users(self):
+        pass
 
     @classmethod
     def model(cls, ns, *args, **kwargs):
         return ns.model("ChatRoom", {
-
+            "cid": fields.String(title="聊天室ID"),
+            "name": fields.String(title="聊天室名"),
+            "path": fields.String(title="聊天室路径"),
+            "owner": fields.String(title="聊天室所有者"),
+            "users": fields.List(fields.String(), title="用户列表")
         })
 
 
--- ifplus/chat/views/__init__.py
+++ ifplus/chat/views/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
+from ifplus.restful.patched import Namespace
 
+namespace = Namespace('chats',
+                      title='即时消息API',
+                      version='1.0',
+                      description='即时消息 RestFul API',
+                      tags='chats')
 chats_blueprint = Blueprint('chats', __name__)
 
 from . import events
