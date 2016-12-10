da47d484abf2aef98abcab98b12b11e753364a26
CaoKe <hitakaken@gmail.com>
2016-12-10 21:50:24 +0800

    Comment Moded Added

--- ifplus/res/models/comment.py
+++ ifplus/res/models/comment.py
@@ -5,6 +5,7 @@ import random
 import string
 from bson import ObjectId
 from pymongo import IndexModel, ASCENDING, DESCENDING
+from ifplus.restful.patched import fields
 
 
 def comment(fid, text, parent_slug=None, user=None, ts=None):
@@ -30,3 +31,14 @@ MONGO_INDEXES = [
     IndexModel([(u'fid', ASCENDING), (u'slug', ASCENDING)], name=u'by_slug'),
     IndexModel([(u'fid', ASCENDING), (u'ts', DESCENDING)], name=u'by_timestamp'),
 ]
+
+COMMENT_REST_MODEL = {
+    u'user': fields.Nested({
+        u'sid': fields.String(),
+        u'name': fields.String(),
+        u'avator': fields.String()
+    }),
+    u'text': fields.String(),
+    u'ts': fields.DateTime(),
+    u'slug': fields.String()
+}
